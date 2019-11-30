2b43f130a5ec66df830253cfb4cd355623d440e8
onuonum <57856659+onuonum@users.noreply.github.com>
2019-11-30 14:24:29 +0900

    検索画面を作成しました。

--- frontend/src/my-place/src/app/geo-search/geo-search.component.ts
+++ frontend/src/my-place/src/app/geo-search/geo-search.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
+export interface Address {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-geo-search',
   templateUrl: './geo-search.component.html',
   styleUrls: ['./geo-search.component.css']
 })
 export class GeoSearchComponent implements OnInit {
+  addresses: Address[] = [
+    {value: 'kyoto', viewValue: '京都市'},
+    {value: 'kameoka', viewValue: '亀岡市'},
+    {value: 'uji', viewValue: '宇治市'}
+  ];
   constructor() { }
 
   ngOnInit() {
--- frontend/src/my-place/src/app/geo-search/geo-search.module.ts
+++ frontend/src/my-place/src/app/geo-search/geo-search.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 import { GeoSearchComponent } from './geo-search.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [GeoSearchComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class GeoSearchModule { }
