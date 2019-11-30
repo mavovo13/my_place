e758d050d77e877adddb572eb41eb8b02d86b959
onuonum <57856659+onuonum@users.noreply.github.com>
2019-11-30 13:40:28 +0900

    トップページを成形しました
    * スクロールが出ないようにした
    * module化した
    * cardを調整した
    * titleの場所を調整した

--- frontend/src/my-place/src/app/app.module.ts
+++ frontend/src/my-place/src/app/app.module.ts
@@ -1,30 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapSearchComponent } from './map-search/map-search.component';
-import { TopPageComponent } from './top-page/top-page.component';
-import { GeoSearchModule } from './geo-search/geo-search.module';
-import { FavoriteSearchComponent } from './favorite-search/favorite-search.component';
-
+import { TopPageModule } from './top-page/top-page.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapSearchComponent,
-    TopPageComponent,
-    FavoriteSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatCardModule,
-    BrowserAnimationsModule,
-    GeoSearchModule
+    TopPageModule,
  ],
   providers: [],
   bootstrap: [AppComponent]
