4f5b4b924dc869e3d24d25ac0d544f8e7edb2496
平田将之 <hiratamasayuki@hiratamasayukinoMacBook-Air.local>
2019-12-02 20:39:04 +0900

    docker起動時にWebサーバを立ち上げるようにしました1

--- python/apitest.py
+++ python/apitest.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+  
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
